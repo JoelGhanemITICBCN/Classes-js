caa72ca8dcc2fef90dad84223d943b7e12c91211
Joel Ghanem <cf22joel.ghanem@iesjoandaustria.org>
2024-01-27 22:36:05 +0100

    Ya elimina bien

--- Projecte-Gestio_de_cicles/Gestio_de_cicles/script.js
+++ Projecte-Gestio_de_cicles/Gestio_de_cicles/script.js
@@ -17,6 +17,7 @@ function afegirCicle() {
         llistatCicles.push(cicle);
     } else {
         //Editar cicle
+    cicle = { nom: nom, categoria: categoria, numAlumnes: numAlumnes, abreviatura: abreviatura }
 
     }
 
@@ -48,9 +49,13 @@ function afegirModul() {
     console.log("llistatModuls");
     console.log(llistatModuls);
 
+    //No se si vols imprimir els moduls o no 
+
+    /*
     actualitzarSelector();
     printLlistat(llistatModuls);
     //Netegem els formularis
+    */
     netejarFormularis();
 }
 
@@ -58,21 +63,56 @@ function printLlistat(llistat) {
     let str = "";
     console.log(llistat);
     llistat.forEach(function (element, index) {
+        //Para printear los modulos por pantalla
+        /*if ('hores' in element) {
+            str += `<div class="block p-6 mb-3 w-full bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">${element.cicle.toUpperCase()}. ${element.nom}</h5>
+                        <h6 class="text-gray-700">${element.num}</h6>
+                        <p class="font-normal text-gray-700">Hores: ${element.hores}</p>
+                    </div>`;
+        } else {
+            */
         str += `<div class="block p-6 mb-3 w-full bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">${element.abreviatura.toUpperCase()}. ${element.nom}</h5>
-                    <h6 class="text-gray-700">${element.categoria}</h6>
-                    <p class="font-normal text-gray-700">Num d'alumnes: ${element.numAlumnes}</p>
-
-                    <button id="btnEliminarCicle" type="button" data-index="${index}" class="eliminar focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Eliminar</button>
-                    <button type="button" data-index="${index}" class="editar focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Editar</button>
-                    <button type="button" data-index="${index}" class="calcular focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Càlcul hores</button>
-                </div>`;
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">${element.abreviatura.toUpperCase()}. ${element.nom}</h5>
+                        <h6 class="text-gray-700">${element.categoria}</h6>
+                        <p class="font-normal text-gray-700">Num d'alumnes: ${element.numAlumnes}</p>
+                    </div>`;
+        //}
+        str += `<button type="button" id="btnEliminarCicle${index}" data-index="${index}"class="eliminar focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Eliminar</button>
+        <button type="button" id="btnEditaCicle${index}" data-index="${index}" class="editar focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Editar</button>
+        <button type="button" id="btnCalculaHores${index}"  data-index="${index}" class="calcular focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Càlcul hores</button>`;
+ 
+   
     });
     let llistatElement = document.getElementById("llistat");
     llistatElement.innerHTML = "";
     llistatElement.innerHTML = str;
+    setTimeout(addEventListeners(llistat), 0);
 }
+     function addEventListeners(llistat){
+        llistat.forEach(function (element,index) {
+     document.getElementById(`btnEliminarCicle${index}`).addEventListener('click', function (event) {
+            console.log("llistat");
+            console.log(llistat);
+            console.log("index");
+            console.log(index);
+            console.log('Eliminar', event.target.dataset.index);
+            removeCicle(index);
+            actualitzarSelector();
+            printLlistat(llistat);
+        });
 
+        document.getElementById(`btnEditaCicle${index}`).addEventListener('click', function (event) {
+            editCicle(index);
+            console.log('editar', event.target.dataset.index);
+        });
+
+        document.getElementById(`btnCalculaHores${index}`).addEventListener('click', function (event) {
+            calculHores();
+            console.log('Calcular horas', event.target.dataset.index);
+        });
+    });
+    }
 
 //Funció per actualitzar el selector de cicles cada vegada que afegim un cicle
 function actualitzarSelector() {
@@ -87,13 +127,22 @@ function actualitzarSelector() {
 }
 
 //Funció per eliminar un cicle
-function removeCicle(i) {
+function removeCicle(index) {
+    /*
     console.log("se ha llamado al elimina");
-    let index = llistatCicles.indexOf(i);
-    if (index != -1) {
+    console.log(index);
+    console.log("llistatCicles antes de borrar");
+    console.log(llistatCicles);
+    */
+    if (index >= 0 && index < llistatCicles.length) {
         llistatCicles.splice(index, 1);
     }
+    /*
+    console.log("llistatCicles dsps de borrar");
+    console.log(llistatCicles);
+    */
 }
+
 
 //Funció per editar un cicle
 function editCicle(i) {
@@ -145,6 +194,7 @@ class Cicle {
     }
 
     calculHores() {
+        console.log("calculHores");
         return this.moduls.reduce((total, modul) => total + modul.hores, 0);
     }
 }
@@ -166,6 +216,7 @@ class Modul {
 }
 let btnAfegirCicle = document.getElementById('btnAfegirCicle');
 let btnAfegirModul = document.getElementById('btnAfegirModul');
+let btnCalculaHores = document.getElementById('btnCalculaHores');
 document.addEventListener('DOMContentLoaded', (event) => {
     btnEliminarCicle = document.getElementById('btnEliminarCicle');
 });
@@ -173,6 +224,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 btnAfegirCicle.addEventListener('click', afegirCicle);
 btnAfegirModul.addEventListener('click', afegirModul);
+btnCalculaHores.addEventListener('click', calculHores);
 if (btnEliminarCicle !== "undefined") {
     btnEliminarCicle.addEventListener('click', function () { removeCicle(this.nom); });
 }
